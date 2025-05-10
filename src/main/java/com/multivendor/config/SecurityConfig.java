12e18ca62c57f5b68cd1a59c7b1674331c3da437
GhaithSomrani <ghaithd02@gmail.com>
2025-05-11 00:54:23 +0100

    hot fixesss

--- src/main/java/com/multivendor/config/SecurityConfig.java
+++ src/main/java/com/multivendor/config/SecurityConfig.java
@@ -1,4 +1,3 @@
-// Fixed SecurityConfig.java
 package com.multivendor.config;
 
 import org.springframework.context.annotation.Bean;
@@ -23,18 +22,5 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
-    }
-}
-
-// Application main class
-package com.multivendor;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MultiVendorApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MultiVendorApplication.class, args);
     }
 }
--- src/main/java/com/multivendor/repository/OrderDetailRepository.java
+++ src/main/java/com/multivendor/repository/OrderDetailRepository.java
@@ -1,3 +1,6 @@
+
+package com.multivendor.repository;
+
 import com.multivendor.model.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
